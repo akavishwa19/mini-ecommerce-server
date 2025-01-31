25b52d8edf8ceb108f1aa07a91e4fb6e053bcd2e
Akash <akash19102001@gmail.com>
2025-01-31 13:37:53 +0530

    small issue

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -71,7 +71,7 @@ const login = async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET,{expiresIn:'5h'});
 
-    res.cookie("auth_cookie", token, { maxAge: 5 * 60 * 60 * 1000, httpOnly: true });
+    res.cookie("auth_cookie", token, { maxAge: 5 * 60 * 60 * 1000, httpOnly: true, sameSite: "None" , secure: true, });
 
     return res
       .status(200)
